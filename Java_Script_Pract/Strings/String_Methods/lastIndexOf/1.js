c48585c1ee3c17eb0558f6210aef83892b2bdab2
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-03-28 16:33:49 +0530

    't'

---
+++ Java_Script_Pract/Strings/String_Methods/lastIndexOf/1.js
@@ -0,0 +1,8 @@
+let str = "Programming";
+
+let substr = "g";
+console.log(str.lastIndexOf(substr));
+
+let result = str.lastIndexOf(substr);
+
+console.log(result);
---
+++ Java_Script_Pract/Strings/String_Methods/lastIndexOf/2.js
@@ -0,0 +1,5 @@
+let str = 'Programming';
+
+let substr = 'g';
+
+console.log(str.lastIndexOf(substr,6));
