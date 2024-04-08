c7f4748ea3a8e3ae7b8efbe678e938cfde8e38ec
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-08 15:22:19 +0530

    't'

---
+++ Exercises/Part_1/5.js
@@ -0,0 +1,11 @@
+const accnum = 4567789;
+let code = "kota5678";
+let accHolName  = "Suoer Star";
+let balance = 5000;
+let bankadd = "Some where";
+
+console.log(accnum);
+console.log(code);
+console.log(accHolName);
+console.log(balance);
+console.log(bankadd);
