67090db44d44612755ce26a8ee6d4a5f1e871ed9
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-08 15:17:54 +0530

    't'

---
+++ Exercises/Part_1/3.js
@@ -0,0 +1,8 @@
+console.log(2 === 2);
+console.log(2 == "2");
+console.log(2 === +"2");
+console.log("learnjavascript" == "learnjavascript");
+console.log("LearnJavascript" == "learnjavascript");
+console.log(0.3 == 0.3);
+console.log(1.0 == 1);
+console.log(1.0 === 1);
---
+++ Exercises/Part_1/4.js
@@ -0,0 +1,4 @@
+console.log(1 + 1);
+console.log(1 + 200);
+console.log(200 + "300");
+console.log("learnjavascript" + 400);
