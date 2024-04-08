02051d6202c81b940838766683b9c451dfc33b19
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-08 16:02:09 +0530

    't'

---
+++ Exercises/Part2/10.js
@@ -0,0 +1,5 @@
+let year = 2001;
+let today = 2024;
+let age = today - year;
+
+console.log(age);
---
+++ Exercises/Part2/9.js
@@ -0,0 +1,9 @@
+let marks = 880;
+let totalMarks = 1000;
+let percentage = marks / totalMarks * 100;
+console.log(percentage);
+if(percentage > 70){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
