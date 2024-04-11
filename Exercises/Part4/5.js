df5b14b6fc89421e9aedb4b301fb20998ea2fb1b
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-11 23:36:24 +0530

    't'

---
+++ Exercises/Part4/5.js
@@ -0,0 +1,5 @@
+let x = Number(prompt("How many time do you want to print ?"));
+
+for(let i = 0; i < x; i++){
+    console.log("Hello World!");
+}
---
+++ Exercises/Part4/9.js
@@ -0,0 +1,6 @@
+let num = 1;
+
+while( num <= 2048){
+    console.log(num);
+    num *= 2;
+}
