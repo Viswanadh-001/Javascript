ad92848deb1f3978b4734987687d4ddda47647ac
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-12 11:47:17 +0530

    't'

--- Exercises/Part4/18.js
+++ Exercises/Part4/18.js
@@ -1,9 +1,9 @@
-const number = 70;
+const num = 70;
 
 console.log("Numbers by which 70 is divisible:");
 
-for (let i = 1; i <= number; i++) {
-    if (number % i === 0) {
+for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
         console.log(i);
     }
 }
