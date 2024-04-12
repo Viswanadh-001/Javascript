fec6fc7f6c56fcb74992e7281bd3a75354ab2d8f
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-12 08:50:13 +0530

    't'

--- Exercises/Part4/12.js
+++ Exercises/Part4/12.js
@@ -1,13 +1,13 @@
 for(let i = 1; i <= 50; i++){
     if(i <= 10){
-        console.log(i + "A");
+        console.log(i + " A");
     }else if(i <= 20){
-        console.log(i + "B");
+        console.log(i + " B");
     }else if(i <= 30){
-        console.log(i + "C");
+        console.log(i + " C");
     }else if(i <= 40){
-        console.log(i + "D");
+        console.log(i + " D");
     }else if(i <= 50){
-        console.log(i +"E")
+        console.log(i +" E")
     }  
 }
---
+++ Exercises/Part4/13.js
@@ -0,0 +1,13 @@
+for(let i = 50; i >= 1; i--){
+    if(i <= 10){
+        console.log(i + " E");
+    }else if(i <= 20){
+        console.log(i + " D");
+    }else if(i <= 30){
+        console.log(i + " C");
+    }else if(i <= 40){
+        console.log(i + " B");
+    }else if(i <= 50){
+        console.log(i +" A")
+    }  
+}
