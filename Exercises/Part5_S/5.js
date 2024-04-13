2385587d50107dad4fa4f441e23383a0ecb85d4a
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-13 18:19:13 +0530

    't'

---
+++ Exercises/Part5_S/5.js
@@ -0,0 +1,8 @@
+let str = "hello";
+let str2 = "world";
+
+let cap = str.charAt(0).toUpperCase() + str.slice(1);
+let cap2 = str2.charAt(0).toUpperCase() + str2.slice(1);
+
+console.log(cap);
+console.log(cap2);
