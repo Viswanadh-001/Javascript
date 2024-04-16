0ffc2943a760356d86d80ab8af931c75a7e8c93e
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-16 08:22:04 +0530

    't'

---
+++ Exercises/Part6/9.js
@@ -0,0 +1,11 @@
+//let str = prompt("Enter a String: ");
+let str = "Rajinikanth";
+let strlen = str.length;
+
+if(strlen > 10){
+    for(let i = 1; i <= strlen; i++){
+        console.log(i);
+    }
+}else{
+    console.log("NO");
+}
