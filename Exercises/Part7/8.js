18c70d8af2b5f0d02fe2c7bb17f868d1cfb82cc5
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-04-17 19:39:43 +0530

    't'

---
+++ Exercises/Part7/8.js
@@ -0,0 +1,9 @@
+let word = "Ajith Kumar";
+let guess = prompt("What is the character you are guessing?");
+let index = prompt("What is the index?");
+
+if(word.charAt(index) === guess){
+    console.log("Congratulations");
+}else{
+    console.log("Sorry");
+}
